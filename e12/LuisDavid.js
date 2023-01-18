39eb987c5174775092d6a24b32781d97aedb6f4e
AlejandroLap235 <bilko69.alar@gmail.com>
2023-01-18 20:05:03 +0100

    Proyecto-clase-abstracta-Alejandro-actualizado-Luis-David-revisado-Miguel-Angel

--- e12/LuisDavid.js
+++ e12/LuisDavid.js
@@ -1,4 +1,4 @@
-import Triangulo from "./e12.js";
+import {Triangulo} from "./e12.js";
 
-console.log(Triangulo.area(10,5));
-console.log(Triangulo.perimetro(10));
+console.log("Area: "+Triangulo.area(10,5));
+console.log("Perimetro:"+Triangulo.perimetro(10));
